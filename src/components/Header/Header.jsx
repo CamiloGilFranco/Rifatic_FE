280ab3667c01600de0eca1380c03d402b62d1c87
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-08-17 15:54:57 -0500

    se agrega botton de cambio de tema

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import styles from "./Header.module.scss";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
+import sun from "../../assets/light_theme.svg";
+import moon from "../../assets/dark_theme.svg";
 
 const Header = () => {
+  const [theme, setTheme] = useState(false);
   return (
     <div className={styles.container}>
       <div className={styles.logo_container}>
@@ -13,6 +17,14 @@ const Header = () => {
         <span className={styles.button}>Iniciar sesión</span>
         <span className={styles.button}>Regístrate</span>
         <span className={styles.button}>Donar</span>
+        <Image
+          src={theme ? sun : moon}
+          width={30}
+          height=""
+          alt="dark theme"
+          onClick={() => setTheme(!theme)}
+          className={styles.theme_button}
+        />
       </div>
     </div>
   );
