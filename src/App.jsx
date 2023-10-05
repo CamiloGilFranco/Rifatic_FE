5c2e8a9322f81bcb52d95e5ecc1ad7e05dd2ead7
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-10-05 11:33:33 -0500

    se crea componente MainStepsBanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import styles from "./App.module.scss";
 import Header from "./components/Header/Header";
 import MainBannerComponent from "./components/MainBannerComponent/MainBannerComponent";
+import MainStepsBannerComponent from "./components/MainStepsBannerComponent/MainStepsBannerComponent";
 
 function App() {
   return (
     <div>
       <Header />
       <MainBannerComponent />
+      <MainStepsBannerComponent />
     </div>
   );
 }
---
+++ src/components/MainStepsBannerComponent/MainStepsBannerComponent.jsx
@@ -0,0 +1,43 @@
+import styles from "./MainStepsBannerComponent.module.scss";
+import img1 from "../../assets/cash1.svg";
+import img2 from "../../assets/cash2.svg";
+import img3 from "../../assets/cash3.svg";
+
+const MainStepsBannerComponent = () => {
+  return (
+    <div className={styles.main_steps_banner}>
+      <div className={styles.single_container}>
+        <div className={styles.image_container}>
+          <img src={img1} alt="" className={styles.item_image} />
+        </div>
+        <h2 className={styles.item_subtitle}>Crea tu talonario</h2>
+        <p className={styles.item_text}>
+          en RIFATIC crea tus talonarios gratis y compártelos con tus amigos
+          para maximizar tus ventas
+        </p>
+      </div>
+      <div className={styles.single_container}>
+        <div className={styles.image_container}>
+          <img src={img2} alt="" className={styles.item_image} />
+        </div>
+        <h2 className={styles.item_subtitle}>Crea tu talonario</h2>
+        <p className={styles.item_text}>
+          en RIFATIC crea tus talonarios gratis y compártelos con tus amigos
+          para maximizar tus ventas
+        </p>
+      </div>
+      <div className={styles.single_container}>
+        <div className={styles.image_container}>
+          <img src={img3} alt="" className={styles.item_image} />
+        </div>
+        <h2 className={styles.item_subtitle}>Crea tu talonario</h2>
+        <p className={styles.item_text}>
+          en RIFATIC crea tus talonarios gratis y compártelos con tus amigos
+          para maximizar tus ventas
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MainStepsBannerComponent;
