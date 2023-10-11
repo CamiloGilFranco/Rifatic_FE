c1d5ade7a583527c9509c5de60b7aa1848562a2a
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-10-11 07:58:04 -0500

    se crea pagina de inicio de sesion con su formulario y se intala toastify y sweetalert pero esta ultima con una vulnerabilidad no resuelta

--- src/components/LogInFormComponent/LogInFormComponent.jsx
+++ src/components/LogInFormComponent/LogInFormComponent.jsx
@@ -1,7 +1,48 @@
 import styles from "./LogInFormComponent.module.scss";
+import logo from "../../assets/logo.png";
+import { useState } from "react";
 
 const LogInFormComponent = () => {
-  return <div className={styles.log_in_form_component}>LogInFormComponent</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.log_in_form_component}>
+      <img src={logo} alt="" className={styles.logo} />
+      <h2 className={styles.title}>Iniciar Sesión</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label htmlFor="" className={styles.label}>
+          Email
+        </label>
+        <input
+          type="text"
+          className={styles.input}
+          value={email}
+          id="log-in-email-input"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="" className={styles.label}>
+          Contraseña
+        </label>
+        <input
+          type="password"
+          className={styles.input}
+          value={password}
+          id="log-in-password-input"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="submit"
+          className={styles.submit_button}
+          value={"Iniciar Sesión"}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default LogInFormComponent;
