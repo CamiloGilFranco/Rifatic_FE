e2344b0d24baae1ab9c94bbb31766030470141de
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-10-10 23:46:44 -0500

    se crea formulario de registro

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import styles from "./App.module.scss";
 import { Routes, Route } from "react-router-dom";
 import { routes } from "./constants/routes";
 import Home from "./pages/Home/Home";
+import Registrate from "./pages/Registrate";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path={routes.home} element={<Home />} />
+        <Route path={routes.registrate} element={<Registrate />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Registrate.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import RegisterFormComponent from "../components/RegisterFormComponent/RegisterFormComponent";
+
+const Registrate = () => {
+  return (
+    <div>
+      <Header />
+      <RegisterFormComponent />
+      <Footer />
+    </div>
+  );
+};
+
+export default Registrate;
