ad49b7861063098edf989aa9bd3358902eff8ec9
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-10-26 00:24:58 -0500

    se actualiza user nab var

--- src/components/UserNavBarComponent/UserNavBarComponent.jsx
+++ src/components/UserNavBarComponent/UserNavBarComponent.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import styles from "./UserNavBarComponent.module.scss";
 
 const UserNavBarComponent = () => {
-  return <div>UserNavBarComponent</div>;
+  return (
+    <div className={styles.user_nav_bar}>
+      <span className={styles.user_name}>Adrian Camilo Gil Franco</span>
+      <div className={styles.nav_buttons_container}>
+        <button className={styles.nav_button}>Reportar Problema</button>
+        <button className={styles.nav_button}>Cerrar Sesión</button>
+        <button className={styles.nav_button}>Mi Perfil</button>
+        <button className={styles.nav_button}>Crear Sorteo</button>
+      </div>
+    </div>
+  );
 };
 
 export default UserNavBarComponent;
