05c3f15ff1e81f58a3fa7f33e134072ed7a1fd6c
Adrian Camilo Gil <adriang@digitalwareadvisory.com>
2024-08-05 18:27:46 -0500

    se crea componente de registraar resultado

--- src/components/AdminResults/AdminResults.jsx
+++ src/components/AdminResults/AdminResults.jsx
@@ -1,49 +1,11 @@
 import { useState } from "react";
 import styles from "./AdminResults.module.scss";
-import DatePicker from "react-datepicker";
+import RecordResult from "./RecordResult/RecordResult";
 
 const AdminResults = () => {
-  const [date, setDate] = useState(null);
-
   return (
     <div className={styles.admin_results}>
-      <div className={styles.record_result_container}>
-        <span className={styles.title}>Registrar Resultado</span>
-        <div className={styles.draw_options_container}>
-          <div className={styles.date_picker_container}>
-            <label htmlFor="" className={styles.input_label}>
-              Fecha de Sorteo
-            </label>
-            <DatePicker
-              showIcon
-              selected={date}
-              onChange={(date) => setDate(date)}
-              className={styles.date_picker}
-              minDate={new Date()}
-              placeholderText="00/00/0000"
-              dateFormat="dd/MM/yyyy"
-            />
-          </div>
-          <div className={styles.lottery_selector_container}>
-            <label htmlFor="" className={styles.input_label}>
-              Lotería
-            </label>
-            <select className={styles.lottery_selector}></select>
-          </div>
-        </div>
-        <div className={styles.winner_number_container}>
-          <label htmlFor="" className={styles.input_label}>
-            Número Ganador
-          </label>
-          <div className={styles.winner_number_inputs_container}>
-            <input type="text" className={styles.winner_number_input} />
-            <input type="text" className={styles.winner_number_input} />
-            <input type="text" className={styles.winner_number_input} />
-            <input type="text" className={styles.winner_number_input} />
-          </div>
-        </div>
-      </div>
-
+      <RecordResult />
       <div className={styles.last_results_container}>
         <span className={styles.title}>Últimos Resultados</span>
       </div>
---
+++ src/components/AdminResults/RecordResult/RecordResult.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import styles from "./RecordResult.module.scss";
+import DatePicker from "react-datepicker";
+
+const RecordResult = () => {
+  const [date, setDate] = useState(null);
+
+  return (
+    <div className={styles.record_result_container}>
+      <span className={styles.title}>Registrar Resultado</span>
+      <div className={styles.draw_options_container}>
+        <div className={styles.date_picker_container}>
+          <label htmlFor="" className={styles.input_label}>
+            Fecha de Sorteo
+          </label>
+          <DatePicker
+            selected={date}
+            onChange={(date) => setDate(date)}
+            className={styles.date_picker}
+            placeholderText="00/00/0000"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+        <div className={styles.lottery_selector_container}>
+          <label htmlFor="" className={styles.input_label}>
+            Lotería
+          </label>
+          <select className={styles.lottery_selector}>
+            <option value="- - -">Elige Una</option>
+            <option value="Lotería de Cundinamarca">
+              Lotería de Cundinamarca
+            </option>
+            <option value="Lotería de La Cruz Roja">
+              Lotería de La Cruz Roja
+            </option>
+            <option value="Lotería del Meta">Lotería del Meta</option>
+            <option value="Lotería de Bogotá">Lotería de Bogotá</option>
+            <option value="Lotería de Medellin">Lotería de Medellin</option>
+            <option value="Lotería de Boyacá">Lotería de Boyacá</option>
+          </select>
+        </div>
+      </div>
+      <div className={styles.winner_number_container}>
+        <label htmlFor="" className={styles.input_label}>
+          Número Ganador
+        </label>
+        <div className={styles.winner_number_inputs_container}>
+          <input type="text" className={styles.winner_number_input} />
+          <input type="text" className={styles.winner_number_input} />
+          <input type="text" className={styles.winner_number_input} />
+          <input type="text" className={styles.winner_number_input} />
+        </div>
+      </div>
+      <button className={styles.record_winner_button}>Registrar Ganador</button>
+    </div>
+  );
+};
+
+export default RecordResult;
